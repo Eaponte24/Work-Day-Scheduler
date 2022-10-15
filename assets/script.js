50d343cb2249b70455fb9373a23b5f856f833c02
Ricky Aponte <Rickyapontephotography@gmail.com>
2022-10-15 13:23:37 -0400

    created the changing ccolor function to change colors for the different time blocks

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,40 @@
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do YYYY, h:mm a"));
+
+
+    
+    // changes colors of the rows based off time
+function colorChanging() {
+var currentHour = moment().hour();
+    
+$(".time-block").each(function(){
+    var timeBlock = parseInt($(this).attr("id").split("hour")[1]);
+
+        if (timeBlock < currentHour) {
+            
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+            $(this).addClass("past");
+
+        } else if (timeBlock === currentHour) {
+            
+            $(this).removeClass("future");
+            $(this).removeClass("past");
+            $(this).addClass("present");
+
+        } else {
+            
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+        }
+    
+    }) 
+
+};
+
+        
+    
+
+   
+
